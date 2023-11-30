bffb103b5970c267ae7fdd5d6a1de25028df65bf
tktyo <zhaohaichao24@outlook.com>
2023-11-30 13:26:53 +0900

    add lc349 solution

---
+++ HashTable/lc349.py
@@ -0,0 +1,13 @@
+# intersection of Two Arrays
+class Solution:
+    def intersection(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        table = [-1] * 1001
+        result = []
+        for i in nums1:
+            table[i] = 1
+        for i in nums2:
+            if table[i] == 1:
+                table[i] += 1
+                if table[i] == 2:
+                    result.append(i)
+        return result
